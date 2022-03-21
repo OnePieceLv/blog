9909b91443d55e7fe9cfaf1736c4dc96b7c2dbfc
steven lv <steven.suzhou@gmail.com>
2022-03-21 20:42:13 +0800

    fix: rename

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "morpheus lv's Blog",
+  title: "morpheus 的日志",
   tagline: '',
   url: 'https://lvwei.blog',
   baseUrl: '/',
@@ -55,7 +55,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: "morpheus lv's Blog",
+        title: "morpheus 的日志",
         logo: {
           src: "https://cdn.ifun.pub/%E9%93%B6%E6%97%B6.jpg?roundPic/radius/400",
           width: 32,
